28580efa416f01944e9fd73ca8fa509269383b52
EC2 Default User <ec2-user@ip-172-31-31-214.us-east-2.compute.internal>
2018-05-03 11:03:29 +0000

    수정완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,39 +1,36 @@
 class HomeController < ApplicationController
   def index
-    post= Post.all
+    @post = Post.all
   end
 
   def create
     @post = Post.new
-    @post.title = params[:post_content]
-    @post.content = params[:post_title]
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
     @post.save 
-    
-    redirect_to 'home/index'
+    redirect_to '/index'
   end
 
   def new
+    
   end
   
   def destroy 
     post = Post.find(params[:post_id])
     post.destroy
-    
-    redirect_to '/home/index'
-    
+    redirect_to '/index'
   end
   
   def edit 
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
   end
   
   def update 
-    post= Post.find(params[:id])
+    post= Post.find(params[:post_id])
     post.title = params[:post_title]
     post.content = params[:post_content]
     post.save 
-    
-    redirect_to '/home/index'
+    redirect_to '/index'
   end
   
-
+end
